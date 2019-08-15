53e0e994b2e14dc9d37800ed6bbf654b0ea8a2df
Andrei Orlov <aaorlov1@gmail.com>
2019-08-15 20:50:38 +0400

    add grandpa

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -2,7 +2,7 @@ use primitives::{ed25519, sr25519, Pair};
 use akropolisos_substrate_node_runtime::{
 	AccountId, Perbill, Permill, Schedule, GenesisConfig, ConsensusConfig, TimestampConfig, BalancesConfig,
 	SudoConfig, IndicesConfig, SessionConfig, StakingConfig, DemocracyConfig, CouncilVotingConfig,
-	TreasuryConfig, ContractConfig
+	GrandpaConfig, TreasuryConfig, ContractConfig
 };
 use substrate_service;
 
@@ -118,7 +118,7 @@ fn testnet_genesis(initial_authorities: Vec<AuthorityId>, endowed_accounts: Vec<
 		}),
 		session: Some(SessionConfig {
 			validators: endowed_accounts.clone(),
-			keys: endowed_accounts.iter().cloned().zip(initial_authorities).collect(),
+			keys: endowed_accounts.iter().cloned().zip(initial_authorities.clone()).collect(),
 			session_length: 6
 		}),
 		staking: Some(StakingConfig {
@@ -145,6 +145,9 @@ fn testnet_genesis(initial_authorities: Vec<AuthorityId>, endowed_accounts: Vec<
 			cooloff_period: 360, // Period (in blocks) that a veto is in effect.
 			voting_period: 60, // Period (in blocks) that a vote is open for.
 			enact_delay_period: 5, // Number of blocks by which to delay enactment of successful.
+		}),
+		grandpa: Some(GrandpaConfig {
+			authorities: initial_authorities.iter().cloned().map(|x| (x, 1)).collect()
 		}),
 		treasury: Some(TreasuryConfig {
 			proposal_bond: Permill::from_millionths(50_000), // Proportion of funds that should be bonded in order to place a proposal.
