e27d0a13ef0171be42c022bae9e9cf76e5f59234
alekspickle <aleks.work2222@gmail.com>
2019-09-12 10:20:21 +0300

    token: clean up

--- runtime/src/token.rs
+++ runtime/src/token.rs
@@ -28,7 +28,7 @@ decl_module! {
         pub fn transfer(origin, to: T::AccountId, #[compact] value: T::TokenBalance) -> Result {
             let sender = ensure_signed(origin)?;
             Self::_transfer(sender, to, value)
-        }
+        } 
 
         pub fn approve(origin, spender: T::AccountId, #[compact] value: T::TokenBalance) -> Result {
             let sender = ensure_signed(origin)?;
@@ -43,6 +43,14 @@ decl_module! {
             Ok(())
         }
 
+        pub fn burn(origin, #[compact] value: T::TokenBalance) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            Self::deposit_event(RawEvent::Burn(sender, value));
+
+            Ok(())
+        }
+
         // if approved, transfer from an account to another account without needing owner's signature
         pub fn transfer_from(_origin, from: T::AccountId, to: T::AccountId, #[compact] value: T::TokenBalance) -> Result {
             ensure!(<Allowance<T>>::exists((from.clone(), to.clone())), "Allowance does not exist.");
@@ -50,7 +58,7 @@ decl_module! {
             ensure!(allowance >= value, "Not enough allowance.");
 
             let updated_allowance = allowance.checked_sub(&value).ok_or("overflow in calculating allowance")?;
-            
+
             <Allowance<T>>::insert((from.clone(), to.clone()), updated_allowance);
 
             Self::deposit_event(RawEvent::Approval(from.clone(), to.clone(), value));
@@ -64,8 +72,9 @@ decl_event!(
     pub enum Event<T> where AccountId = <T as system::Trait>::AccountId, TokenBalance = <T as self::Trait>::TokenBalance {
         Transfer(AccountId, AccountId, TokenBalance),
         Approval(AccountId, AccountId, TokenBalance),
+        Burn(AccountId, TokenBalance),
     }
-    );
+);
 
 impl<T: Trait> Module<T> {
     pub fn init(owner: T::AccountId, ) -> Result {
@@ -81,11 +90,11 @@ impl<T: Trait> Module<T> {
         from: T::AccountId,
         to: T::AccountId,
         value: T::TokenBalance,
-        ) -> Result {
+    ) -> Result {
         ensure!(<BalanceOf<T>>::exists(from.clone()), "Account does not own this token");
         let sender_balance = Self::balance_of(from.clone());
         ensure!(sender_balance >= value, "Not enough balance.");
-        let updated_from_balance = sender_balance.checked_sub(&value).ok_or("overflow in calculating balance")?;
+        let updated_from_balance = sender_balance.checked_sub(&value).ok_or("underflow in calculating balance")?;
         let receiver_balance = Self::balance_of(to.clone());
         let updated_to_balance = receiver_balance.checked_add(&value).ok_or("overflow in calculating balance")?;
 
@@ -94,5 +103,83 @@ impl<T: Trait> Module<T> {
 
         Self::deposit_event(RawEvent::Transfer(from, to, value));
         Ok(())
+    }
+}
+
+
+/// tests for this module
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use primitives::{Blake2Hasher, H256};
+    use runtime_io::with_externalities;
+    use runtime_primitives::{
+        testing::{Digest, DigestItem, Header},
+        traits::{BlakeTwo256, IdentityLookup},
+        BuildStorage,
+    };
+    use support::{assert_noop, assert_ok, impl_outer_origin};
+
+    impl_outer_origin! {
+        pub enum Origin for Test {}
+    }
+
+    // For testing the module, we construct most of a mock runtime. This means
+    // first constructing a configuration type (`Test`) which `impl`s each of the
+    // configuration traits of modules we want to use.
+    #[derive(Clone, Eq, PartialEq)]
+    pub struct Test;
+    impl system::Trait for Test {
+        type Origin = Origin;
+        type Index = u64;
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type Digest = Digest;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = ();
+        type Log = DigestItem;
+    }
+    impl balances::Trait for Test {
+        type Balance = u128;
+        type OnFreeBalanceZero = ();
+        type OnNewAccount = ();
+        type TransactionPayment = ();
+        type TransferPayment = ();
+        type DustRemoval = ();
+        type Event = ();
+    }
+    impl timestamp::Trait for Test {
+        type Moment = u64;
+        type OnTimestampSet = ();
+    }
+    impl Trait for Test {
+        type Event = ();
+    }
+    type TokenModule = Module<Test>;
+
+    const TOKEN_NAME: &[u8; 10] = b"NAME";
+    const TOKEN_DESC: &[u8; 10] = b"Description-1234_";
+    const USER1: u64 = 1;
+    const USER2: u64 = 2;
+
+    // This function basically just builds a genesis storage key/value store according to
+    // our desired mockup.
+    fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+        system::GenesisConfig::<Test>::default()
+            .build_storage()
+            .unwrap()
+            .0
+            .into()
+    }
+
+    #[test]
+    fn create_token_init() {
+        with_externalities(&mut new_test_ext(), || {
+            // assert_eq!();
+        })
     }
 }
