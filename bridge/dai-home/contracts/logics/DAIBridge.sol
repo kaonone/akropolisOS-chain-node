251f2c0e44f0939fc9608d8ed5330ec2ef114785
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-09-23 15:27:51 +0300

    Status message+Change Events

--- bridge/dai-home/contracts/logics/DAIBridge.sol
+++ bridge/dai-home/contracts/logics/DAIBridge.sol
@@ -9,15 +9,18 @@ contract DAIBridge is  BeneficiaryOperations {
 
         IERC20 private token;
 
+    enum Status {PENDING,WITHDRAW,APPROVED, CANCELED, CONFIRMED}
+
         struct Message {
             bytes32 messageID;
             address spender;
-            string substrateAddress;
+            bytes32 substrateAddress;
             uint availableAmount;
+            Status status;
         }
 
-        event RelayMessage(bytes32 messageID, address indexed sender, string indexed recipient, uint amount);
-        event WithdrawFromBridge(address indexed sender, uint amount);
+        event RelayMessage(bytes32 messageID, address sender, bytes32 recipient, uint amount);
+        event WithdrawFromBridge(bytes32 MessageID, address sender, uint amount);
 
         mapping(bytes32 => Message) messages;
         mapping(address => Message) messagesBySender;
@@ -61,34 +64,37 @@ contract DAIBridge is  BeneficiaryOperations {
             _;
         }
 
+        modifier pendingMessage(bytes32 messageID) {
+            require(messages[messageID].status ==  Status.PENDING, "Message is not pending");
+            _;
+        }
 
+         modifier approvedMessage(bytes32 messageID) {
+            require(messages[messageID].status ==  Status.APPROVED, "Message is not approved");
+            _;
+        }
 
         /*
         * Set Transfer to Bridge
         */
 
-        function setTransfer(uint amount, string memory substrateAddress) public {
+        function setTransfer(uint amount, bytes32 substrateAddress) public {
             require(token.allowance(msg.sender, address(this)) >= amount, "contract is not allowed to this amount");
             token.transferFrom(msg.sender, address(this), amount);
 
             bytes32 messageID = keccak256(abi.encodePacked(now));
 
-            Message  memory message = Message(messageID, msg.sender, substrateAddress, amount);
+            Message  memory message = Message(messageID, msg.sender, substrateAddress, amount, Status.PENDING);
 
             emit RelayMessage(messageID, msg.sender, substrateAddress, amount);
         }
 
-        function withdraw() public {
-            Message storage message = messagesBySender[msg.sender];
+        function withdraw(bytes32 messageID) public pendingMessage(messageID) {
+            Message storage message = messages[messageID];
 
             token.transfer(msg.sender, message.availableAmount);
 
-            message.availableAmount = 0;
-
-            emit WithdrawFromBridge(msg.sender, message.availableAmount);
+            emit WithdrawFromBridge(messageID, msg.sender, message.availableAmount);
         }
-
-
-
 
 }
