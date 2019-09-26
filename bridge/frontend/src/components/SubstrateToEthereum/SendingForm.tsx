3bd8f9b8de54bcda998ab0a010cba7407255739e
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-09-26 17:47:07 +0700

    Added form for transfer DAI from Substrate to Ethereum

---
+++ bridge/frontend/src/components/SubstrateToEthereum/SendingForm.tsx
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import { useCallback } from 'react';
+import { Form, Field } from 'react-final-form';
+import { FORM_ERROR } from 'final-form';
+import { Button, Typography, MenuItem } from '@material-ui/core';
+
+import { TextField, Select } from '~components/form';
+import { useApi } from '~components/context';
+import { useSubscribable } from '~util/hooks';
+import getErrorMsg from '~util/getErrorMsg';
+
+interface FormData {
+  address: string;
+  amount: string;
+  from: string;
+}
+
+const fields: { [key in keyof FormData]: key } = {
+  address: 'address',
+  amount: 'amount',
+  from: 'from',
+};
+
+function SendingForm() {
+  const api = useApi();
+  const [accounts, { loaded: accountsLoaded }] = useSubscribable(() => api.getSubstrateAccounts$(), []);
+
+  const handleSubmit = useCallback(async ({ from, address, amount }: FormData) => {
+    try {
+      await api.sendToEthereum(from, address, amount);
+    } catch (error) {
+      return { [FORM_ERROR]: getErrorMsg(error) };
+    }
+  }, []);
+
+  if (!accountsLoaded) {
+    return null;
+  }
+
+  if (!accounts || !accounts.length) {
+    return (
+      <Typography color="error">
+        You don't have any Substrate accounts, you need to create an account in the browser extension Polkadot.js
+      </Typography>
+    )
+  }
+
+  return (
+    <Form<FormData>
+      onSubmit={handleSubmit}
+      subscription={{ submitting: true, submitError: true }}
+      initialValues={{ from: accounts[0].address, address: '', amount: '' }}
+    >
+      {({ handleSubmit, submitting, submitError }): React.ReactElement<{}> => (
+        <form onSubmit={handleSubmit}>
+          <Field
+            name={fields.from}
+            component={Select as any}
+            label='From'
+            error={false}
+            InputProps={{
+              autoFocus: true
+            }}
+            InputLabelProps={{
+              shrink: true
+            }}
+            formControlProps={{
+              fullWidth: true,
+              variant: "outlined",
+              margin: "normal"
+            }}
+          >
+            {accounts.map(value => (
+              <MenuItem value={value.address} key={value.address}>{value.meta.name}</MenuItem>
+            ))}
+          </Field>
+          <Field
+            name={fields.address}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            label='To'
+            margin="normal"
+            error={false}
+            InputProps={{
+              autoFocus: true
+            }}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <Field
+            name={fields.amount}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            label='Amount'
+            margin="normal"
+            error={false}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          {!!submitError && <Typography variant='body1' color="error">{submitError}</Typography>}
+          <Button fullWidth type="submit" variant="contained" color="primary" disabled={submitting}>
+            Send{submitting && 'ing'}
+          </Button>
+        </form>
+      )}
+    </Form>
+  );
+}
+
+export default SendingForm;
