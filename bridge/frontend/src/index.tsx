d7afcdb32cac967e201cfee27d991a987cc15e26
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-11-13 12:43:41 +0300

    App version log added

--- bridge/frontend/src/index.tsx
+++ bridge/frontend/src/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Root } from 'core/Root';
+import { getEnv } from 'core/getEnv';
 
 function render(component: React.ReactElement): void {
   ReactDOM.render(component, window.document.getElementById('root'));
 }
 
 render(<Root />);
+
+console.info(`App version is ${getEnv().appVersion}`);
