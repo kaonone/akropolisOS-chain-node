704de299fb73ca88b0777a22c09f8309fb5aaddc
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2020-03-27 10:18:41 +0200

    set up repo build

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,4 +1,3 @@
-
 //! Substrate CLI library.
 //!
 //! This package has two Cargo features:
@@ -20,9 +19,9 @@ mod browser;
 #[cfg(feature = "cli")]
 mod cli;
 #[cfg(feature = "cli")]
-mod factory_impl;
-#[cfg(feature = "cli")]
 mod command;
+#[cfg(feature = "cli")]
+mod factory_impl;
 
 #[cfg(feature = "browser")]
 pub use browser::*;
@@ -34,41 +33,59 @@ pub use command::*;
 /// The chain specification option.
 #[derive(Clone, Debug, PartialEq)]
 pub enum ChainSpec {
-	/// Whatever the current runtime is, with just Alice as an auth.
-	Development,
-	/// Whatever the current runtime is, with simple Alice/Bob auths.
-	LocalTestnet,
-	/// Whatever the current runtime is with the "global testnet" defaults.
-	AkropolisOSStaging,
-	/// Akropolis OS Mainnet
+    /// Whatever the current runtime is, with just Alice as an auth.
+    Development,
+    /// Whatever the current runtime is, with simple Alice/Bob auths.
+    LocalTestnet,
+    /// Whatever the current runtime is with the "global testnet" defaults.
+    AkropolisOSStaging,
+    /// Akropolis OS Mainnet
     AkropolisOS,
 }
 
 /// Get a chain config from a spec setting.
 impl ChainSpec {
-	pub(crate) fn load(self) -> Result<chain_spec::ChainSpec, String> {
-		Ok(match self {
-			ChainSpec::Development => chain_spec::development_config(),
-			ChainSpec::LocalTestnet => chain_spec::local_testnet_config(),
-			ChainSpec::AkropolisOSStaging=> chain_spec::staging_testnet_config(),
-			ChainSpec::AkropolisOS=> chain_spec::staging_testnet_config(),
-		})
-	}
+    pub(crate) fn load(self) -> Result<chain_spec::ChainSpec, String> {
+        Ok(match self {
+            ChainSpec::Development => chain_spec::development_config(),
+            ChainSpec::LocalTestnet => chain_spec::local_testnet_config(),
+            ChainSpec::AkropolisOSStaging => chain_spec::staging_testnet_config(),
+            ChainSpec::AkropolisOS => chain_spec::staging_testnet_config(),
+        })
+    }
 
-	pub(crate) fn from(s: &str) -> Option<Self> {
-		match s {
-			"dev" => Some(ChainSpec::Development),
-			"local" => Some(ChainSpec::LocalTestnet),
-			"" | "akro" | "akropolisos" => Some(ChainSpec::AkropolisOS),
-			"staging" => Some(ChainSpec::AkropolisOSStaging),
-			_ => None,
-		}
-	}
+    pub(crate) fn from(s: &str) -> Option<Self> {
+        match s {
+            "dev" => Some(ChainSpec::Development),
+            "local" => Some(ChainSpec::LocalTestnet),
+            "" | "akro" | "akropolisos" => Some(ChainSpec::AkropolisOS),
+            "staging" => Some(ChainSpec::AkropolisOSStaging),
+            _ => None,
+        }
+    }
 }
 
 fn load_spec(id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
-	Ok(match ChainSpec::from(id) {
-		Some(spec) => Box::new(spec.load()?),
-		None => Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(id))?),
-	})
+    Ok(match ChainSpec::from(id) {
+        Some(spec) => Box::new(spec.load()?),
+        None => Box::new(chain_spec::ChainSpec::from_json_file(
+            std::path::PathBuf::from(id),
+        )?),
+    })
+}
+
+pub fn run_cli() -> sc_cli::Result<()> {
+    use std::env;
+    let version = sc_cli::VersionInfo {
+        name: "AkropolisOS",
+        commit: env!("VERGEN_SHA_SHORT"),
+        version: env!("CARGO_PKG_VERSION"),
+        executable_name: "akropolisos-node",
+        author: "Akropolis",
+        description: "Akropolis OS Node",
+        support_url: "admin@akropolis.io",
+        copyright_start_year: 2017,
+    };
+
+    crate::run(env::args(), version)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
 //! Substrate Node CLI
 #![warn(missing_docs)]
 
-use std::env;
-// use dotenv::dotenv;
 fn main() -> sc_cli::Result<()> {
-	// dotenv().ok();
-	let version = sc_cli::VersionInfo {
-		name: "AkropolisOS",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "akropolisos-node",
-		author: "Akropolis",
-		description: "Akropolis OS Node",
-		support_url: "admin@akropolis.io",
-		copyright_start_year: 2017,
-	};
-
-	node_cli::run(env::args(), version)
+	node_cli::run_cli()
 }
